c59ca9b6c07252e90c3df8930550ca1a3f78534b
MateoPerez2 <101563507+MateoPerez2@users.noreply.github.com>
2022-04-05 23:08:19 -0300

    Finished tp2

---
+++ Assets/panel/ValidarContraseña.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    public Text ingresoUsuario;
+    string contraseniaUsiario;
+    public GameObject cartelitoMsj;
+    public Text textoMsj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ValidarContrasenia()
+        {
+        contraseniaUsiario = ingresoUsuario.text;
+        if (contraseniaUsiario == contraseniaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña Incorrecta";
+            Debug.Log("Contraseña Incorrecta");
+        }
+        }
+
+}
